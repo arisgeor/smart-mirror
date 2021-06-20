bc9d8fcb60d0692e42823438726647a2f7cce8fd
arisgeor <30298541+arisgeor@users.noreply.github.com>
2021-06-20 12:06:15 +0300

    improved comments and made slight changes to some function names

--- main.py
+++ main.py
@@ -1,57 +1,70 @@
-from Tkinter import *  # python2
-import tkMessageBox  # python2
+from Tkinter import *   # python2
+import tkMessageBox     # python2
 import sqlite3 as sql
 import test_fing
 import test_scale as Scale
 from mlx90614.get_temp import get_temp
 from max3010x.heart_main import heart_sensor as HRM
 from datetime import datetime
-con = sql.connect('finger_users.db')
+con = sql.connect('finger_users.db')            # Creates Database 
 cur = con.cursor()
 cur.execute(
     "Create Table IF NOT EXISTS Users (User_id number primary key, E_Date Date);");
 
-cur.execute(
+cur.execute(                                    # Creates Table
     "Create Table IF NOT EXISTS User_data (User_id number, E_Date Date, heart_rate number, sp02 number, temp number, weight number);");
 
 
 
 def toggle_window(event):
+    ''' Exit Fullscreen mode when you press the Esc key '''
     window.attributes("-fullscreen", False)
     window.geometry("1224x650+50+0")
 
 
-bgcolor = 'Black'
+bgcolor = 'Black'               # Background colors for the different frames used.
 bg_color1 = 'Black'
-text_color = 'white'  # text colors
-font_name = 'Time New Roman'  # font name
+text_color = 'white'            # text colors
+font_name = 'Time New Roman'    # font name
 
-window = Tk()  # Window variable that holds the App.
-window.title("Title")  # Title
+window = Tk()                           # Window variable that holds the entire App.
+window.title("Smart Mirror")            # Application Title appearint at the top of the window
 window.attributes("-fullscreen", True)
-window.config(background=bgcolor)  # Applying background color in window.
-window.bind('<Escape>', toggle_window)  # Use the Escape Button to exit Fullscreen Mode.
+window.config(background=bgcolor)       # Applying background color in window.
+window.bind('<Escape>', toggle_window)  # Use the Escape Button to exit Fullscreen Mode through toggle_window().
 
 home_page_title = "Smart-Mirror"
-home_page_heading = "Biometric Measuring Device"
+home_page_motto = "Biometric Measuring Device"
+footer_text = "Contact me: arisgeor@ece.auth.gr"
 
 global after_v, user_id, values
 values=[]
 
-def back():
+def home():
+    ''' 
+    Function that returns the User to the Home (main) screen,
+    and saves the recorded biomarkers in the Database.
+    '''
     global after_v
-    main_frame.pack(pady=30)
+    main_frame.pack(pady=30)                
     print(after_v)
     print('Cancel')
-    Body_frame.pack_forget()
+    Body_frame.pack_forget()                
     window.after_cancel(after_v)
     print(str(values[0]),'Date',str(values[1]),str(values[2]),str(values[-2]),str(values[-1]))
     cur_date=datetime.now().strftime('%y-%m-%d %H:%M:%S')
     cur.execute("insert into User_data (user_id,E_date,Heart_rate,sp02,temp,weight) values (?,?,?,?,?,?)",(values[0],cur_date,values[1],values[2], values[-2], values[-1]))
     con.commit()
-    print('data saved into DB Successfully...')
-#### test again
+    print('Data saved into DB Successfully...')
+
 def Test_again(u_id):
+    ''' 
+    Function that executes after user clicks the "Test Again" Button.
+    Lets the user repeat the measuremenent and reaquire his/her biomarkers, 
+    without returning to the HOME tab.
+    This function takes as an argument the user's ID, in order to repeat the measurement
+    and save the Data under the same User. 
+    '''
     global values
     tkMessageBox.showinfo('Info', "Place you finger on Heart Scanner.... \nand press ok...")
     values=[]
@@ -59,15 +72,13 @@ def Test_again(u_id):
     Sp02_value=round(HRM_data[1],2)
     Heart_rate.config(text='Heart-rate : ' + str(HRM_data[0]))
     Sp02.config(text='Sp02 : ' + str(Sp02_value))
-    #cur.execute('update users set heart_rate=?, sp02=? where user_id=?',(HRM_data[0], HRM_data[1], u_id))
-    #con.commit()
-    #print('data save successfully... !')
     values.append(u_id)
     values.append(HRM_data[0])
     values.append(Sp02_value)
     print(values)
+
 def menu_bar(window_name):
-    ''' Menu Bar of the Application (Top Left)'''
+    ''' Menu Bar of the Application (Top Left) Containing usufull options'''
 
     my_menu = Menu(window_name)
     window_name.config(menu=my_menu)
@@ -76,49 +87,54 @@ def menu_bar(window_name):
     window_menu.add_command(label="Maximize", command=lambda: window.attributes('-fullscreen', True))
     window_menu.add_separator()
     window_menu.add_command(label="Press Esc Key to Minimize",
-                            command=lambda: tkMessageBox.showinfo('Info', "Press Esc Key to Minimize"))
+                            command=lambda: tkMessageBox.showinfo('Info', "Press Esc Key to Minimize")) ###########
     window_menu.add_separator()
-    window_menu.add_command(label="Back", command=lambda: back())
+    window_menu.add_command(label="Back", command=lambda: home())
     window_menu.add_separator()
     window_menu.add_command(label="Exit", command=lambda: window.quit())
 
 
-def Header_part(window_name, Title, Heading):
-    ''' Header function of the Application '''
+def Header_part(window_name, Title, Motto):
+    ''' 
+    Function that sets the header part of the Application.
+    Creates the logo and text frames and places the 3 labels
+    containing the Title, Motto and Date of the App. 
+    '''
 
-    logo_frame = Frame(window_name, bg=bgcolor)
+    logo_frame = Frame(window_name, bg=bgcolor)     
     logo_frame.pack(side=TOP, fill='x')
     logo_text_frame = Frame(logo_frame, bg=bgcolor)
     logo_text_frame.pack(side=LEFT, padx=5)
+    # Labels
     Label(logo_text_frame, text=Title, font=(font_name, 28, 'bold', 'italic'), bg=bgcolor,
           fg=text_color).pack(side=TOP, anchor=NW, pady=5)
-    Label(logo_text_frame, text=Heading,
+    Label(logo_text_frame, text=Motto,
           font=(font_name, 16), bg=bgcolor, fg=text_color).pack(side=TOP, pady=2)
-    # Labels
     today_date = datetime.now().strftime('%d-%m-%Y (%A)')
     logo_label = Label(logo_frame, text=today_date, bg=bgcolor, fg=text_color, font=(font_name, 28, 'bold', 'italic'))
     logo_label.pack(side=RIGHT, anchor=NE, padx=5)
     Canvas(window_name, height=2, bg='Red').pack(fill='x')
 
-
 def footer(window_name, footer_text):
-    ''' Footer function of the Application '''
+    ''' 
+    Footer function of the Application. 
+    Places the footer_text at the bottom of the screen.
+    '''
 
     Label(window_name, text=footer_text, font=(font_name, 12), justify=LEFT, bg=bgcolor, fg=text_color).pack(
         side=BOTTOM, anchor=NE, padx=30)
     Canvas(window_name, height=2, bg='Blue').pack(side=BOTTOM, fill='x', pady=5)
 
 
-# ***************************
-# ************ Main Window ************
-# ***************************
+# ************************************* #
+# ************ Main Window ************ #
+# ************************************* #
 
 menu_bar(window)
-Header_part(window, home_page_title, home_page_heading)
-main_frame = Frame(window, bg=bgcolor)  # Create the "HOME" main_frame.
-main_frame.pack(pady=30)  # place it.
-# Label(main_frame, text='HOME', font=(font_name, 20, 'bold', 'italic'), justify=LEFT, bg=bgcolor, pady=20,
-#      fg=text_color).pack(side=TOP, pady=10)
+Header_part(window, home_page_title, home_page_motto)
+main_frame = Frame(window, bg=bgcolor)                  # Creates the "HOME" main_frame.
+main_frame.pack(pady=30)                                # places it.
+
 Button(main_frame, text='Login', font=(font_name, 12, 'bold'), width=15, bg='black', fg=text_color, bd=3,
        command=lambda: body_code()).pack(side=TOP, pady=10)
 Button(main_frame, text='Add User', font=(font_name, 12, 'bold'), width=15, bg='black', fg=text_color, bd=3,
@@ -130,15 +146,15 @@ Button(main_frame, text='Power Off', font=(font_name, 12, 'bold'), width=15, bg=
 Button(main_frame, text='Window maximize', font=(font_name, 12, 'bold'), width=15, bg='black', fg=text_color, bd=3,
        command=lambda: window.attributes('-fullscreen', True)).pack(side=TOP, pady=10)
 
-footer(window, 'Contact me: arisgeor@ece.auth.gr')
+footer(window, footer_text)
 
-# For when user logs in:
-Body_frame = Frame(window, bg=bgcolor)  # Body_frame is created within window but not placed yet.
-# It will be placed when the user successfuly logs in.
-User_frame = Frame(Body_frame, bg=bgcolor)  # User_frame is created within Body_frame
+# After User logs in:
+Body_frame = Frame(window, bg=bgcolor)      # Body_frame is created within window but not placed yet.
+                                            # It will be placed when the user successfuly logs in.
+User_frame = Frame(Body_frame, bg=bgcolor)  # User_frame is created within Body_frame.
 User_frame.pack(side=LEFT, padx=30)
 
-Text_frame = Frame(Body_frame, bg=bgcolor)  # Text_frame is created within Body_Frame
+Text_frame = Frame(Body_frame, bg=bgcolor)  # and Text_frame is created within Body_Frame.
 Text_frame.pack(side=RIGHT, padx=100)
 
 # Inside User_frame:
@@ -146,7 +162,7 @@ User_id_lb = Label(User_frame, text='User Id: ????', font=(font_name, 20, 'bold'
                    fg=text_color)
 User_id_lb.pack(side=TOP, pady=10)
 Button(User_frame, text='Home', font=(font_name, 12, 'bold'), width=15, bg='gray', fg=text_color, bd=3,
-       command=lambda: back()).pack(side=TOP, pady=10)
+       command=lambda: home()).pack(side=TOP, pady=10)
 Button(User_frame, text='Test Again', font=(font_name, 12, 'bold'), width=15, bg='gray', fg=text_color, bd=3,
        command=lambda: Test_again(user_id)).pack(side=TOP, pady=10)
 
@@ -168,31 +184,31 @@ except ValueError as e:
 
 
 def body_code():
-    global user_id
-    ''' App Screen After User Clicks the "Log In" Button '''
+    ''' 
+    App Screen After User Clicks the "Log In" Button 
+    Contains all possible scenarios of Verification 
+    and pops up the approporiate message
+    '''
 
-    main_frame.pack_forget()  # Hide the "Home Screen" and pop up the message below.
+    global user_id
+    main_frame.pack_forget()                                # Hides the "Home Screen" and pops up the message below.
     Place_sensor_lb = Label(window, text='Place your finger on the sensor.....', bg=bgcolor, fg=text_color,
                             font=(font_name, 45))
-    #Place_sensor_lb.pack()
     r = test_fing.VerifyUser()
     print(r)
     user_id=r[1]
-    #cur.execute('Select * from Users where user_id=?', (r[1],))
-    #res = cur.fetchall()
-    if r[1] >= 0:  # User is successfully verified.
+    if r[1] >= 0:                                           # User is successfully verified.
         Place_sensor_lb.pack_forget()
-        User_id_lb.config(text='User Id = ' + str(r[1]))  # Hide the pop up notification.
-        Body_frame.pack(side=TOP, pady=30, fill='x')  # place the Body_frame.
+        User_id_lb.config(text='User Id = ' + str(r[1]))    # Hides the pop up notification.
+        Body_frame.pack(side=TOP, pady=30, fill='x')        # Places the Body_frame containing the measurements.
 
         if True:
             Test_again(user_id)
             show_values_of_sensors()
-    else:
-        Place_sensor_lb.pack_forget()  # Hide the pop up notification.
-        Body_frame.pack_forget()  # Hide the Body_frame.
-        main_frame.pack(pady=30)  # Put the main_frame back in place.
-        # Verification failed.
+    else:                                                   # Verification failed.
+        Place_sensor_lb.pack_forget()                       # Hides the pop up notification.
+        Body_frame.pack_forget()                            # Hides the Body_frame.
+        main_frame.pack(pady=30)                            # Puts the main_frame back in place.
         if r[1] == -1:
             tkMessageBox.showwarning('Failed', "Time Out... !")
         elif r[1] == -3:
@@ -224,19 +240,8 @@ def show_values_of_sensors():
 
 def add_users():
     '''
-    new_window = Toplevel()
-    new_window.title("New User")  # Title
-    new_window.config(background=bgcolor)  # Applying background color in window
-    new_window.geometry('500x400+450+100')
-    Label(new_window, text='Place your finger on the scanner after that press start.....')
-    frame1 = LabelFrame(new_window, text='Add User', font=(font_name, 18, 'bold'), bg=bgcolor, fg=text_color, padx=80,
-                        pady=10)
-    frame1.pack(pady=50)
-    Label(frame1, text='User ID', font=(font_name, 12, 'bold', 'italic'), justify=LEFT, bg=bgcolor,
-          fg=text_color).pack(side=TOP, pady=2)
-    User_id = Entry(frame1, font=(font_name, 12), bg='gray', fg=text_color, bd=3)
-    User_id.pack(pady=2)
-    footer(new_window, 'Contact us | Copyright by your name or company name')
+    Function that initiates the procedure of adding a 
+    new user to the Smart Mirror.
     '''
 
     response = tkMessageBox.askyesno("Confirmation",
@@ -247,7 +252,7 @@ def add_users():
         print('The id is =>  ', r[1])
         cur.execute('select * from users where user_id=?;', (r[1],))
         res = cur.fetchall()
-        if len(res) > 0:
+        if len(res) > 0:                            # User already exists.
             tkMessageBox.showwarning("Failed:", 'User Id already exists !')
         else:
             if r[1] >= 0:
@@ -264,9 +269,9 @@ def add_users():
 
 
 def Del_all_user():
-    ''' Delets all the Users from the Application's database, as well as the Fingerprint sensor's '''
+    ''' Delets all the Users from the Application's as well as the Fingerprint sensor's Databases'''
 
-    response = tkMessageBox.askyesno("Warning", "This will erase all the User id's do you want to continue...")
+    response = tkMessageBox.askyesno("Warning", "This will erase all the User id's do you wish to continue?")
     if response == 1:
         test_fing.ClearAllUser()
         cnt_user = test_fing.GetUserCount()
