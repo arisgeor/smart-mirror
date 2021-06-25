17517287ab2ef7ae6857654458009a0d93c3b366
arisgeor <30298541+arisgeor@users.noreply.github.com>
2021-06-25 14:32:10 +0300

    tried to improve database visuals

--- get_data.py
+++ get_data.py
@@ -3,7 +3,6 @@ import tkMessageBox as messagebox  # python2
 
 
 def user_data(user_id):
-    #inner = ''
     data = ''
     con = sql.connect('finger_users.db')
     cur = con.cursor()
@@ -12,8 +11,7 @@ def user_data(user_id):
     res = cur.fetchall()
     if len(res)>0:
         for i in res:
-            data += 'Date: ' + str(i[1]) + ', Heart rate: ' + str(i[2]) + ', SpO2: ' + str(i[3]) + ', Temp: ' + str(i[4]) + ', Weight: ' + str(i[5]) + '/n'
-            print data
+            data += 'Data Measured on: ' + str(i[1]) + '\nHeart rate: ' + str(i[2]) + '\nSpO2: ' + str(i[3]) + '\nTemp: ' + str(i[4]) + '\nWeight: ' + str(i[5]) + '\n\n'
     	messagebox.showinfo("User " + str(user_id) + " Data", data)
     else:
         messagebox.showinfo("User " + str(user_id) + " Data", "No Data Found...... ")
