683b814921e3fdce2443291dc91809a848f90d8f
arisgeor <30298541+arisgeor@users.noreply.github.com>
2021-06-25 13:51:03 +0300

    changed database get method

--- get_data.py
+++ get_data.py
@@ -3,12 +3,16 @@ import tkMessageBox as messagebox  # python2
 
 
 def user_data(user_id):
+    #inner = ''
+    data = ''
     con = sql.connect('finger_users.db')
     cur = con.cursor()
     cur.execute(
               "Select * from User_data where user_id=?;", (user_id,));
     res = cur.fetchall()
     if len(res)>0:
-    	messagebox.showinfo("Data !", res)
+        for i in res:
+            data += 'Date: ' + res[i][1] + ', Heart rate: ' + res[i][2] + ', SpO2: ' + res[i][3] + ', Temp: ' + res[i][4] + ', Weight: ' + res[i][5] + '/n'
+    	messagebox.showinfo("User " + user_id + " Data", data)
     else:
-        messagebox.showinfo("Data !", "No Data Found...... ")
+        messagebox.showinfo("User " + user_id + " Data", "No Data Found...... ")
